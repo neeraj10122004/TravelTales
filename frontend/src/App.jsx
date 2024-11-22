c09a4d90490c3ce82a10925adb10a986a3dce3e5
neeraj10122004 akula <neerajsaiteja@gmail.com>
2024-11-22 06:59:16 +0000

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,43 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Loading } from './pages/Loading'
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
+import { Userpage } from './pages/Userpage'
 
 function App() {
+  const [check, setcheck] = useState(true)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setcheck(!check)
+    }, 1000); 
+    return () => clearTimeout(timer);
+  }, []);
+
 
   return (
     <>
-      <div>Travel Tales</div>
+      {check && <div><Loading/></div>}
+      {!check &&
+      <div>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/userpage" element={<Userpage/>} />
+      </Routes>
+      </Router>
+      </div>
+      }
+      
     </>
   )
 }
 
 export default App
+
