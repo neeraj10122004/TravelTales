1089b2083b7d7eea7a138c8c4df9cf9cdbdf0121
neeraj10122004 akula <neerajsaiteja@gmail.com>
2024-12-08 05:18:19 +0000

    makepost

--- frontend/src/components/Makepost.jsx
+++ frontend/src/components/Makepost.jsx
@@ -3,12 +3,9 @@ import { useState } from 'react';
 
 export const Makepost = ({mail, makepost, setmakepost }) => {
   const [description, setdescription] = useState("")
-  const m = useRef([]);
+  const [lables, setlables] = useState(['trip', 'travel']);
+  const [temp, settemp] = useState("")
 
-  const handleClick = () => {
-    myList.current.push('new item');
-    console.log(myList.current); // Output: ['new item']
-  };
   useEffect(() => {
     const timer = setTimeout(() => {
       setmakepost(false); // Set makepost to false after 2 seconds
@@ -17,14 +14,26 @@ export const Makepost = ({mail, makepost, setmakepost }) => {
     // Cleanup function to clear timeout if the component is unmounted before the timeout
     return () => clearTimeout(timer);
   }, [setmakepost]); // Depend on setmakepost to handle the state correctly
+  const handleclick = ()=>{
+    if(temp.length !==0 ){
+      setlables([...lables,temp])
+      settemp("")
+    }
+  }
+  const sendpost = async () =>{
+    
+  }
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg w-96 flex flex-col justify-center items-center">
       <h2 className="text-2xl font-bold text-center mb-4">Create a Post</h2>
-      <div>
-          <textarea name="" id="" value={description} onChange={(e)=>{
+      <div className='flex flex-col justify-center items-center'>
+          <textarea name=""  placeholder='write post' id="" value={description} onChange={(e)=>{
             setdescription(e.target.value)
           }}></textarea>
+          <input type="text" placeholder="enter labels" name="" id="" value={temp} onChange={(e)=>{settemp(e.target.value)}} />
+          <button type="button" onClick={handleclick}>add label</button>
+          <button type="button" onClick={sendpost}>post</button>
       </div>
           
     </div>
